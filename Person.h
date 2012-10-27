9ca7abd53b0194b746b7ae489302b6eb5657e890
Minsok Oh <ohminsok.co@gmail.com>
2012-10-27 14:47:28 -0400

    initialcommit

---
+++ Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  git
+//
+//  Created by Mac on 10/27/12.
+//  Copyright (c) 2012 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Person : NSObject
+
+@end
---
+++ Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  git
+//
+//  Created by Mac on 10/27/12.
+//  Copyright (c) 2012 Mac. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
